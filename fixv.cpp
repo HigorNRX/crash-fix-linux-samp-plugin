f5cd536415f8265c80d6d4b5048314467fb43340
Galo <85417556+HigorNRX@users.noreply.github.com>
2026-02-16 04:30:27 -0300

    build 2

--- fixv.cpp
+++ fixv.cpp
@@ -1,3 +1,25 @@
+/*___________________________________________________
+|$$$$$$\   $$$$$$\  $$\       $$$$$$\       __///__ |\    
+|$$  __$$\ $$  __$$\ $$ |     $$  __$$\     / 0_0 \ | \                                                                                         
+|$$/   \__|$$ /  $$ |$$ |     $$ /  $$ |    \  V  / |  |   
+|$$|  --. _.uS$$$$Sp, $ |     $$ |  $$ | ((_/     \ |  |              
+|$$| ´_,d´   ?$$$$$$Si. |     $$ |  $$ |  \ <     V)|  |
+|\$  d$$°"?$$$7°°4$$$SIi $$$$\ $$$$$$ /    \______/ |  |
+ \_  `$L\ ,$$`    $$?SII: ____|\______/_______|_|___|  |
+,.d$/  ?$$s$$k_  j$´iISSI `-:                 " "    \ |
+?$$$$L/´?$$SS$$$$?:iISSIi ^  ._                       \|   
+ `4$$$k,/´ `?$$7:iIISSI:    ^       Contatos:
+  `?$$k_d? '$iiLLSI?;:'   ^  ^ :.      Discord: galodebota
+__//\\//\///\\//////\\/\________ .. _  github: https://github.com/Higornrx 
+                                 \_  ':           _... 
+Autor Original: HigorNrx - Galo   \  :.       .::´
+by Galo HigorNRX 16/02/2026       / ; `;\    :::: 
+                                  \  ;  \    ::::`-:._ 
+                                   `-:._`-:._ :::`-:._-
+                                       --___-_--_-\_-´  */
+
+
+
 #define SAMP_SDK_IMPLEMENTATION
 #define SAMP_SDK_WANT_AMX_EVENTS
 #define SAMP_SDK_WANT_PROCESS_TICK
@@ -14,7 +36,7 @@ using namespace Samp_SDK;
 cell SAMP_SDK_CDECL Hook_GetConsoleVarAsString(AMX* amx, cell* params)
 {
     // Bloqueia a execução retornando 0
-    Log("[SA-MP SDK Plugin] GetConsoleVarAsString foi bloqueado!");
+    Log("[VenixHostingFix] GetConsoleVarAsString foi bloqueado!");
     return 0;
 }
 
@@ -22,19 +44,68 @@ cell SAMP_SDK_CDECL Hook_GetConsoleVarAsString(AMX* amx, cell* params)
 PLUGIN_NATIVE_HOOK_REGISTRATION(GetConsoleVarAsString)
 
 // ============================================================================
+// Hook para interceptar GetConsoleVarAsInt
+// Retorna 0 para bloquear a execução da função nativa
+// ============================================================================
+
+cell SAMP_SDK_CDECL Hook_GetConsoleVarAsInt(AMX* amx, cell* params)
+{
+    // Bloqueia a execução retornando 0
+    Log("[VenixHostingFix] GetConsoleVarAsInt foi bloqueado!");
+    return 0;
+}
+
+// Registra o hook automáticamente
+PLUGIN_NATIVE_HOOK_REGISTRATION(GetConsoleVarAsInt)
+
+// ============================================================================
+// Hook para interceptar GetConsoleVarAsBool
+// Retorna 0 para bloquear a execução da função nativa
+// ============================================================================
+
+cell SAMP_SDK_CDECL Hook_GetConsoleVarAsBool(AMX* amx, cell* params)
+{
+    // Bloqueia a execução retornando 0
+    Log("[VenixHostingFix] GetConsoleVarAsBool foi bloqueado!");
+    return 0;
+}
+
+// Registra o hook automáticamente
+PLUGIN_NATIVE_HOOK_REGISTRATION(GetConsoleVarAsBool)
+
+// ============================================================================
+// Hook para interceptar GetConsoleVarAsFloat
+// Retorna 0 para bloquear a execução da função nativa
+// ============================================================================
+
+cell SAMP_SDK_CDECL Hook_GetConsoleVarAsFloat(AMX* amx, cell* params)
+{
+    // Bloqueia a execução retornando 0
+    Log("[VenixHostingFix] GetConsoleVarAsFloat foi bloqueado!");
+    return 0;
+}
+
+// Registra o hook automáticamente
+PLUGIN_NATIVE_HOOK_REGISTRATION(GetConsoleVarAsFloat)
+
+// ============================================================================
 // Funções de callback do plugin (implementadas conforme SDK espera)
 // ============================================================================
 
+
 bool OnLoad()
 {
-    Log("[SA-MP SDK Plugin] Plugin carregado com sucesso!");
-    Log("[SA-MP SDK Plugin] GetConsoleVarAsString foi intercetado e bloqueado!");
+    Log("[VenixHostingFix] Plugin carregado com sucesso!");
+    Log("[VenixHostingFix] GetConsoleVarAsString foi intercetado e bloqueado!");
+    Log("[VenixHostingFix] GetConsoleVarAsInt foi intercetado e bloqueado!");
+    Log("[VenixHostingFix] GetConsoleVarAsBool foi intercetado e bloqueado!");
+    Log("[VenixHostingFix] GetConsoleVarAsFloat foi intercetado e bloqueado!");
     return true;
 }
 
 void OnUnload()
 {
-    Log("[SA-MP SDK Plugin] Plugin descarregado!");
+    Log("[VenixHostingFix] Plugin descarregado!");
 }
 
 unsigned int GetSupportFlags()
@@ -44,15 +115,15 @@ unsigned int GetSupportFlags()
 
 void OnAmxLoad(AMX* amx)
 {
-    // Chamado quando um script é carregado
+    
 }
 
 void OnAmxUnload(AMX* amx)
 {
-    // Chamado quando um script é descarregado
+    
 }
 
 void OnProcessTick()
 {
-    // Chamado a cada frame do servidor
+    
 }
